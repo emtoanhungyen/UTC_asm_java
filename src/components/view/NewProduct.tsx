bcbf77865a50be437900ff291133f7c0264848c7
emtoanhungyen <phamtoannee1506@gmail.com>
2024-10-26 12:25:30 +0700

    Display products on the home page

--- src/components/view/NewProduct.tsx
+++ src/components/view/NewProduct.tsx
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import ImgN1 from "./../../images/n1.jpg";
-import ImgN2 from "./../../images/n2.jpg";
-import ImgN3 from "./../../images/n3.jpg";
-import ImgN4 from "./../../images/n4.jpg";
+import { fetchProducts } from "../../redux/products/action";
+import { IProduct } from "../../redux/products/productSlide";
+import { useAppDispatch, useAppSelector } from "../../store/store";
+import { capitalizeFirstLetter } from "../../utils/capitalizeFirstLetter";
+import "./../../css/styleImage.css";
 type Props = {};
 
 const NewProduct = (props: Props) => {
+  const dispatch = useAppDispatch();
+  const listProducts = useAppSelector((state) => state.product.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
   return (
     <div className="container">
-      <>
-        <h3 className="text-center mt-4 pt-5">Sản phẩm mới</h3>
-        <div className="row mt-5">
+      <h3 className="text-center mt-4 pt-5">Sản phẩm mới</h3>
+      <div className="row mt-5">
+        {listProducts.map((item: IProduct, index) => (
           <div className="col-md-6 col-lg-3 mb-4">
             <div className="">
-              <img className="img-fluid w-100" src={ImgN1} alt="Sample" />
+              <img className="image-style" src={item.image} alt="images" />
               <a href="#!">
                 <div className="mask waves-effect waves-light" />
               </a>
             </div>
             <div className="text-center pt-4">
               <Link to={"/detail"}>
-                <h5 className="mb-0">Hạt Óc Chó hữu cơ</h5>
+                <h5 className="mb-0">{capitalizeFirstLetter(item.name)}</h5>
               </Link>
-              <h5>(1 Hộp)</h5>
-              <h6 className="mb-3">300.000vnd</h6>
+              <h5>({item.content})</h5>
+              <h6 className="mb-3">{item.price}vnd</h6>
               <button
                 type="button"
                 className="btn btn-primary btn-sm mr-1 waves-effect waves-light"
@@ -34,68 +42,8 @@ const NewProduct = (props: Props) => {
               </button>
             </div>
           </div>
-          <div className="col-md-6 col-lg-3 mb-4">
-            <div className="">
-              <img className="img-fluid w-100" src={ImgN2} alt="Sample" />
-              <a href="#!">
-                <div className="mask waves-effect waves-light" />
-              </a>
-            </div>
-            <div className="text-center pt-4">
-              <h5 className="mb-0">Dâu Tây Đà Lạt hữu cơ</h5>
-              <h5>(2 hộp 500g)</h5>
-              <h6 className="mb-3">330.000vnd</h6>
-              <button
-                type="button"
-                className="btn btn-primary btn-sm mr-1 waves-effect waves-light"
-              >
-                <i className="fas fa-shopping-cart pr-2" />
-                Thêm vào giỏ
-              </button>
-            </div>
-          </div>
-          <div className="col-md-6 col-lg-3 mb-4">
-            <div className="">
-              <img className="img-fluid w-100" src={ImgN3} alt="Sample" />
-              <a href="#!">
-                <div className="mask waves-effect waves-light" />
-              </a>
-            </div>
-            <div className="text-center pt-4">
-              <h5 className="mb-0">Táo Mỹ hữu cơ</h5>
-              <h5>(1 kg)</h5>
-              <h6 className="mb-3">149.000vnd</h6>
-              <button
-                type="button"
-                className="btn btn-primary btn-sm mr-1 waves-effect waves-light"
-              >
-                <i className="fas fa-shopping-cart pr-2" />
-                Thêm vào giỏ
-              </button>
-            </div>
-          </div>
-          <div className="col-md-6 col-lg-3 mb-4">
-            <div className="">
-              <img className="img-fluid w-100" src={ImgN4} alt="Sample" />
-              <a href="#!">
-                <div className="mask waves-effect waves-light" />
-              </a>
-            </div>
-            <div className="text-center pt-4">
-              <h5 className="mb-0">Chuối Tây hữu cơ</h5>
-              <h5>(1 kg)</h5>
-              <h6 className="mb-3">99.000vnd</h6>
-              <button
-                type="button"
-                className="btn btn-primary btn-sm mr-1 waves-effect waves-light"
-              >
-                <i className="fas fa-shopping-cart pr-2" />
-                Thêm vào giỏ
-              </button>
-            </div>
-          </div>
-        </div>
-      </>
+        ))}
+      </div>
     </div>
   );
 };
